941609741f22e7b636bbfeb7c3db3212ffe695f1
George Petrov <jokopetrov@gmail.com>
2015-09-27 19:52:27 +0300

    ArenaMarV1

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,27 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-2.1.4.min
+//= require scripts.js
+//= require owl.carousel.min
+//= require jquery_ujs
+//= require jquery-ui-1.10.4.custom.min
+//= require jquery.ba-outside-events.min
+//= require jqueryui
+//= require jquery.vide.min
+//= require tab
+//= require transition
+//= require jquery.matchHeight-min
+//= require modal
+//= require turbolinks
+//= require_self
+//= require_tree .
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,28 @@
+class HomeController < ApplicationController
+
+
+	def index
+		
+	end
+
+	def rooms
+		
+	end
+
+	def gallery
+		
+	end
+
+	def about_us
+		
+	end
+
+	def location
+		
+	end
+
+	def contacts
+		
+	end
+
+end
